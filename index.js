893f8ce01550163d891779cad856592c855f55d2
Scott Westover <scottwestover2006@gmail.com>
2022-12-08 10:26:11 -0500

    chore: testing enhanced logging
    Signed-off-by: Scott Westover <scottwestover2006@gmail.com>

--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const actions = require("./actions");
 async function run() {
   try {
     const config = utils.getConfig();
-    const octokit = github.getOctokit(config.token);
+    const octokit = github.getOctokit(config.token, {
+      log: {
+        debug: (message) => core.info(message),
+        info: (message) => core.info(message),
+        error: (message) => core.info(message),
+        warn: (message) => core.info(message),
+      }
+    });
+    // octokit.rest.packages.deletePackageForOrg({
+    //   package_type: "container",
+    //   package_name: 'maven-content-scanner',
+    //   org: 'lookio',
+    //   package_version_id: '57835353',
+    // });
 
     if (config.tag) {
       await actions.deleteByTag(config, octokit);
